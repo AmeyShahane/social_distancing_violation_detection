b7d8a4bfee4507fe06d82df0aea4778a23d04e7a
Amey Shahane <62330507+AmeyShahane@users.noreply.github.com>
2021-12-02 19:24:28 -0500

    Update sns-lambda.py

--- sns-lambda.py
+++ sns-lambda.py
@@ -14,39 +14,13 @@ def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     
-    # eventname = event['Records'][0]['eventName']
-    # sns_message = str("This Email Represent a File Status has been Changed in One of Your Bucket \n\n BUCKET NAME: "+ bucket +"\n\n FILE NAME: " + key + "\n\n OPERATION: " + eventname + "\n\n")
-    # try:
-    #     print(eventname)
-    #     if eventname == "ObjectRemoved:Delete":
-    #         print("File is being Deleted")
-    #         sns_message += str("File Deleted")
-    #     else:
-    #         response = s3.get_object(Bucket=bucket, Key=key)
-    #         sns_message += str("FILE CONTENT TYPE: " + str(response['ContentType']) + "\n\nFILE CONTENT: " + str(response['Body'].read()))
-    #         print("CONTENT TYPE: " + response['ContentType'])
-    #     print(str(sns_message))
-    #     subject= "S3 Bucket[" + bucket + "] Event[" + eventname + "]"
-    #     print(subject)
-    #     sns_response = sns.publish(
-    #     TargetArn='arn:aws:sns:us-east-1:436809475701:test-project',
-    #     Message= str(sns_message),
-    #     Subject= str(subject)
-    #     )
-    #     #return response['ContentType']
-    # except Exception as e:
-    #     print(e)
-    #     print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-    #     raise e
-    # print("Worked")
-    # location = s3.get_bucket_location(Bucket=bucket)['LocationConstraint']
    
     url = "https://%s.s3.amazonaws.com/%s" % (bucket, key)
     # https://test-term-project.s3.amazonaws.com/SHIVAM+MAHAJAN.jpg
    
     # print(url)
     sns_response = sns.publish(
-        TargetArn='arn:aws:sns:us-east-1:153970823423:crowd_lambda',
+        TargetArn='arn:aws:sns:us-east-1:193337023362:crowd_lambda',
         Message= str(url),
         Subject= "TestSubject"
         )
